2f3fd98ce4a56cbe13d53f987073e80e6b72c9d4
PedrixzZ <131414300+PedrixzZ@users.noreply.github.com>
2024-01-28 18:56:08 -0300

    Update GLFW.java

--- jre_lwjgl3glfw/src/main/java/org/lwjgl/glfw/GLFW.java
+++ jre_lwjgl3glfw/src/main/java/org/lwjgl/glfw/GLFW.java
@@ -1075,7 +1075,7 @@ public class GLFW
     public static void glfwPollEvents() {
         if (!mGLFWIsInputReady) {
             mGLFWIsInputReady = false;
-            CallbackBridge.nativeSetInputReady(false);
+            CallbackBridge.nativeSetInputReady(true);
         }
         callV(Functions.SetupEvents);
         for (Long ptr : mGLFWWindowMap.keySet()) callJV(ptr, Functions.PumpEvents);
